dadf9e6b3161d3f82ea5457bd679ba613f782acf
Asier Del Cid Pérez <103944663+AsierDelCidPerez@users.noreply.github.com>
2023-02-23 17:45:31 +0100

    23/02/2023

--- backend/utils/settings.js
+++ backend/utils/settings.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 
 const port = process.env.PORT || 3001
-const MONGODB_URI = 'mongodb+srv://root:qGYFhp6L4t3RpHSk@cluster0.lvp4y.mongodb.net/asierdelcid?retryWrites=true&w=majority'
-const SECRET = 'y^5ZfiMSVpHY8%dgc%dLQGRgvNb9Q&Cxk^A6@RL^XW^r2spW4k'
+const MONGODB_URI = 'mongodb+srv://root:qGYFhp6L4t3RpHSk@cluster0.lvp4y.mongodb.net/futbolin?retryWrites=true&w=majority'
+const SECRET = 'y^5ZfiMSVpHY8%EHxZ*rSftjR*DW3rzimJY4X&CGC&uJ4k'
 
 module.exports = {
     port, MONGODB_URI, SECRET
